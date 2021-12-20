9a53803e6634d98e5e46bc6f57d07d9507e2db33
Hendi Santika <hendisantika@gmail.com>
2021-12-20 07:53:56 +0700

    #12 - Add SpringDataDialect

--- src/main/java/com/hendisantika/SpringBootMovieTrailersApplication.java
+++ src/main/java/com/hendisantika/SpringBootMovieTrailersApplication.java
@@ -2,6 +2,8 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class SpringBootMovieTrailersApplication {
@@ -10,4 +12,8 @@ public class SpringBootMovieTrailersApplication {
         SpringApplication.run(SpringBootMovieTrailersApplication.class, args);
     }
 
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+    }
 }
