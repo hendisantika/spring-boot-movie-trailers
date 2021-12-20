b8a03feeeb9a590315645686c08a11eb16b0b1a3
Hendi Santika <hendisantika@gmail.com>
2021-12-20 08:18:02 +0700

    #21 - Add listMovies in HomeController

--- src/main/java/com/hendisantika/controller/HomeController.java
+++ src/main/java/com/hendisantika/controller/HomeController.java
@@ -3,8 +3,11 @@ package com.hendisantika.controller;
 import com.hendisantika.model.Movie;
 import com.hendisantika.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +36,12 @@ public class HomeController {
                 movieRepository.findAll(PageRequest.of(0, 4, Sort.by("premiereDate").descending())).toList();
         return new ModelAndView("index")
                 .addObject("latest Movies", latestMovies);
+    }
+
+    @GetMapping("/movies")
+    public ModelAndView listMovies(@PageableDefault(sort = "premiereDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Movie> movies = movieRepository.findAll(pageable);
+        return new ModelAndView("movies")
+                .addObject("movies", movies);
     }
 }
