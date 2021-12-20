0d65b26d736b4f66082e74e8fa0b934336f786a5
Hendi Santika <hendisantika@gmail.com>
2021-12-20 07:47:50 +0700

    #7 - Add FileNotFoundException

---
+++ src/main/java/com/hendisantika/exception/FileNotFoundException.java
@@ -0,0 +1,27 @@
+package com.hendisantika.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-movie-trailers
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/12/21
+ * Time: 07.47
+ */
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class FileNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+
+    public FileNotFoundException(String message, Throwable exception) {
+        super(message, exception);
+    }
+}
