bbbe2bc296294606015d3c48f31babb86e1b6563
Hendi Santika <hendisantika@gmail.com>
2021-12-20 08:22:48 +0700

    #23 - Add AssetsController

---
+++ src/main/java/com/hendisantika/controller/AssetsController.java
@@ -0,0 +1,30 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-movie-trailers
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/12/21
+ * Time: 08.21
+ */
+@RestController
+@RequestMapping("/assets")
+public class AssetsController {
+    @Autowired
+    private WarehouseService warehouseService;
+
+    @GetMapping("/{filename:.+}")
+    public Resource obtenerRecurso(@PathVariable("filename") String filename) {
+        return warehouseService.uploadAsResource(filename);
+    }
+}
