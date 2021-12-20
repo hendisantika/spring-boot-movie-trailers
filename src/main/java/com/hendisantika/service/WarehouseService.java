35a4c44fc79b66abe073b194a0045a8bd708f0d5
Hendi Santika <hendisantika@gmail.com>
2021-12-20 07:56:07 +0700

    #14 - Add startStoreFiles in WarehouseService

--- src/main/java/com/hendisantika/service/WarehouseService.java
+++ src/main/java/com/hendisantika/service/WarehouseService.java
@@ -1,7 +1,13 @@
 package com.hendisantika.service;
 
+import com.hendisantika.exception.WarehouseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +23,14 @@ public class WarehouseService {
     @Value("${storage.location}")
     private String storageLocation;
 
+    //This is used to indicate that this method is going to be executed every time it finds a new instance of this
+    // class.
+    @PostConstruct
+    public void startStoreFiles() {
+        try {
+            Files.createDirectories(Paths.get(storageLocation));
+        } catch (IOException exception) {
+            throw new WarehouseException("Failed to initialize the location in the file store");
+        }
+    }
 }
