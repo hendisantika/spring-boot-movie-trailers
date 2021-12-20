223d4aadd5bc0ec7db1268475559863d5456cefd
Hendi Santika <hendisantika@gmail.com>
2021-12-20 07:39:50 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootMovieTrailersApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovieTrailersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMovieTrailersApplication.class, args);
+    }
+
+}
