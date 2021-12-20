0c72efabcfb5262e5eff929e2c96df69d2b89752
Hendi Santika <hendisantika@gmail.com>
2021-12-20 08:25:05 +0700

    #25 - Add seeHomepage in AdminController

--- src/main/java/com/hendisantika/controller/AdminController.java
+++ src/main/java/com/hendisantika/controller/AdminController.java
@@ -1,11 +1,17 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Movie;
 import com.hendisantika.repository.GenreRepository;
 import com.hendisantika.repository.MovieRepository;
 import com.hendisantika.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +33,11 @@ public class AdminController {
 
     @Autowired
     private WarehouseService warehouseService;
+
+    @GetMapping
+    public ModelAndView seeHomepage(@PageableDefault(sort = "titulo", size = 5) Pageable pageable) {
+        Page<Movie> movies = movieRepository.findAll(pageable);
+        return new ModelAndView("admin/index").addObject("movies", movies);
+    }
+
 }
