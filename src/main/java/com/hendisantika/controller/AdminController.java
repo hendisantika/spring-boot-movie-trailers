cef2b1af07533e235b5172150b31fb97654a875b
Hendi Santika <hendisantika@gmail.com>
2021-12-20 08:26:42 +0700

    #26 - Add showNewFilmForm in AdminController

--- src/main/java/com/hendisantika/controller/AdminController.java
+++ src/main/java/com/hendisantika/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Genre;
 import com.hendisantika.model.Movie;
 import com.hendisantika.repository.GenreRepository;
 import com.hendisantika.repository.MovieRepository;
@@ -7,11 +8,14 @@ import com.hendisantika.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -40,4 +44,11 @@ public class AdminController {
         return new ModelAndView("admin/index").addObject("movies", movies);
     }
 
+    @GetMapping("movies/new")
+    public ModelAndView showNewFilmForm() {
+        List<Genre> genres = genreRepository.findAll(Sort.by("title"));
+        return new ModelAndView("admin/new-movie")
+                .addObject("movie", new Movie())
+                .addObject("genres", genres);
+    }
 }
