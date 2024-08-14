6f6f6380ffde3e68d110ff124bb23a1d0403bc96
hendisantika <hendisantika@yahoo.co.id>
2024-08-14 07:11:48 +0700

    (feat) Add Integration Test 🫸🌀✏️📗🐧🐳

--- src/test/java/com/hendisantika/SpringBootMovieTrailersApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootMovieTrailersApplicationTests.java
@@ -1,13 +1,34 @@
 package com.hendisantika;
 
-import org.junit.jupiter.api.Test;
+import com.hendisantika.repository.GenreRepository;
+import com.hendisantika.repository.MovieRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:8.4.0:///cinemaDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootMovieTrailersApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        genreRepository.deleteAll();
+        movieRepository.deleteAll();
     }
 
 }
